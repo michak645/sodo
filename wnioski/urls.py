14211252bfa14119aea9c1175935adef5f3ddf7d
Michał <michak645@gmail.com>
2017-05-14 17:20:49 +0200

    User login and registration simple version

--- wnioski/urls.py
+++ wnioski/urls.py
@@ -5,5 +5,18 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/', views.add, name='add'),
-    url(r'^list/', views.list, name='list')
+    url(r'^list/', views.list, name='list'),
+
+    url(r'^acc/login', views.login, name='login'),
+    url(r'^acc/authentication', views.authentication, name='authentication'),
+    url(r'^acc/logout', views.logout, name='logout'),
+    url(r'^acc/loggedin', views.loggedin, name='loggedin'),
+    url(r'^acc/invalid', views.invalid, name='invalid'),
+
+    url(r'^acc/create_user$', views.create_user, name='create_user'),
+    url(r'^acc/create_user_succ$', views.create_user_succ,
+        name='create_user_succ'),
+    url(r'^acc/create_user_error', views.create_user_error,
+        name='create_user_error')
+
 ]
--- wnioski/views.py
+++ wnioski/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 from .models import Pracownik, Wniosek, Obiekt
 from .forms import WniosekForm
 from django.utils import formats
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -52,3 +55,54 @@ def list(request):
         'wniosek': wniosek
     }
     return render(request, template, context)
+
+
+def login(request):
+    template = "wnioski/login.html"
+    context = {}
+    return render(request, template, context)
+
+
+def authentication(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/acc/loggedin/')
+    else:
+        return HttpResponseRedirect('/acc/invalid/')
+
+
+def loggedin(request):
+    return render(request, 'wnioski/loggedin.html', {'username': request.user.username})
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'wnioski/logout.html', {})
+
+
+def invalid(request):
+    return render(request, 'wnioski/invalid.html', {})
+
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/acc/create_user_succ')
+        else:
+            return HttpResponseRedirect('/acc/create_user_error')
+    args = {}
+    args['form'] = UserCreationForm()
+    return render(request, 'wnioski/create_user.html', args)
+
+
+def create_user_succ(request):
+    return render(request, 'wnioski/create_user_succ.html', {})
+
+
+def create_user_error(request):
+    return render(request, 'wnioski/create_user_error.html', {})
