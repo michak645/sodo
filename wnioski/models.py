1692e9e20d9238848f79a05d3261e0c05c1b02a3
Micik2 <micik220@gmail.com>
2017-12-02 23:41:13 +0100

    Zamiana TextFields na CharFields

--- wnioski/models.py
+++ wnioski/models.py
@@ -22,7 +22,7 @@ def validate_surname(surname):
 
         
 class Uprawnienia(models.Model):
-    nazwa = models.TextField(unique=True)
+    nazwa = models.CharField(unique=True, max_length=255)
 
     def __str__(self):
         return u'{0}'.format(self.nazwa)
@@ -30,51 +30,55 @@ class Uprawnienia(models.Model):
 
 class JednOrg(models.Model):
     id_jedn = models.CharField(max_length=10, unique=True)
-    nazwa = models.TextField(unique=True)
+    nazwa = models.CharField(unique=True, max_length=255)
 
     def __str__(self):
         return u'{0}'.format(self.nazwa)
 
 
 class TypObiektu(models.Model):
-    nazwa = models.TextField(unique=True)
+    nazwa = models.CharField(unique=True, max_length=255)
 
     def __str__(self):
         return u'{0}'.format(self.nazwa)
 
 
 class Obiekt(models.Model):
-    nazwa = models.TextField()
-    opis = models.TextField()
+    nazwa = models.CharField(max_length=255)
+    opis = models.CharField(max_length=255)
     typ = models.ForeignKey(TypObiektu, on_delete = models.CASCADE)
     jedn_org = models.ForeignKey(JednOrg, on_delete = models.CASCADE)
 
     def __str__(self):
-        return u'{0}'.format(self.nazwa)
+        return u'{0} {1}'.format(self.nazwa, self.opis)
 
 
 class RodzajPracownika(models.Model):
+    nazwa = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return u'{0}'.format(self.nazwa)
+
+        
+class RodzajUzytkownika(models.Model):
     nazwa = models.CharField(max_length=9, unique=True)
 
     def __str__(self):
         return u'{0}'.format(self.nazwa)
 
-
+        
 class Pracownik(models.Model):
     imie = models.CharField(max_length=81, validators=[validate_name])
     nazwisko = models.CharField(max_length=55, validators=[validate_surname])
     email = models.EmailField(null=True, unique=True)
     szkolenie = models.BooleanField(default=False)
-    login = models.CharField(max_length=45, null=True, unique=True)
-    haslo = models.CharField(max_length=45, null=True)
-    rodzaj = models.ForeignKey(RodzajPracownika, on_delete = models.CASCADE, null=True, verbose_name='Rodzaj pracownika')
+    rodzaj_pracownika = models.ForeignKey(RodzajPracownika, on_delete = models.CASCADE, null=True, verbose_name='Rodzaj pracownika')
     jedn_org = models.ForeignKey(JednOrg, on_delete = models.CASCADE, null=True, verbose_name='Jednostka organizacyjna')
-
+    numer_ax = models.CharField(max_length=6, unique=True)
+    czy_pracuje = models.BooleanField(null=True)
+    
     def __str__(self):
-        return u'{0} {1}'.format(
-            self.imie,
-            self.nazwisko
-        )
+        return u'{0} {1} {2}'.format(self.imie, self.nazwisko, self.rodzaj_pracownika)
 
     # class Meta:
     #    order_with_respect_to = 'imie'
@@ -89,12 +93,33 @@ class Pracownik(models.Model):
         instance.pracownik.save()
     '''
 
+class Uzytkownik(models.Model):
+    login = models.CharField(max_length=45, null=True, unique=True)
+    haslo = models.CharField(max_length=45, null=True)
+    rodzaj_uzytkownika = models.ForeignKey(RodzajUzytkownika, on_delete = models.CASCADE, null=True, verbose_name='Rodzaj użytkownika')
+    id_pracownika = models.ForeignKey(Pracownik, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return u'{0}'.format(self.rodzaj_uzytkownika)
+
+    # class Meta:
+    #    order_with_respect_to = 'imie'
+    '''
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Pracownik.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.pracownik.save()
+    '''
 
 class WniosekTyp(models.Model):
-    typ = models.TextField(unique=True)
+    nazwa = models.CharField(unique=True, max_length=255)
 
     def __str__(self):
-        return u'{0}'.format(self.typ)
+        return u'{0}'.format(self.nazwa)
 
 
 class Wniosek(models.Model):
