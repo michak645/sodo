e1d3423281cbab6ef93065083060f0b6f0d205e8
Michał <michak645@gmail.com>
2017-12-09 19:35:18 +0100

    Full front end for user panel

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
-from user_app.views import user_index, user_applications, user_add_app
+from user_app.views import user_index, user_objects, user_add_app, user_app_accepted, user_app_rejected, \
+    user_profile
 
 urlpatterns = [
     url(r'^user_index', user_index, name='user_index'),
-    url(r'^user_applications', user_applications, name='user_applications'),
+    url(r'^user_objects', user_objects, name='user_objects'),
     url(r'^user_add_app', user_add_app, name='user_add_app'),
+    url(r'^user_app_accepted', user_app_accepted, name='user_app_accepted'),
+    url(r'^user_app_rejected', user_app_rejected, name='user_app_rejected'),
+    url(r'^user_profile', user_profile, name='user_profile'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,19 +1,85 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 from auth_ex.models import Pracownik
+from user_app.forms import AddApplicationForm
+from wnioski.models import Historia, Wniosek, Obiekt
 
 
 def user_index(request):
     pracownik = Pracownik.objects.get(id=request.session['pracownik'])
+    wnioski = Wniosek.objects.filter(pracownik=pracownik)
+    historia = Historia.objects.filter(wniosek__in=wnioski, status=3)
     context = {
         'pracownik': pracownik,
+        'historia': historia,
     }
     return render(request, 'user_app/user_index.html', context)
 
 
-def user_applications(request):
-    return render(request, 'user_app/user_applications.html')
+def user_objects(request):
+    pracownik = Pracownik.objects.get(id=request.session['pracownik'])
+    wnioski = Wniosek.objects.filter(pracownik=pracownik)
+    historia = Historia.objects.filter(wniosek__in=wnioski, status=1)
+    obiekty = []
+    for wniosek in historia:
+        obiekty.append(wniosek.wniosek.obiekt)
+    context = {
+        'pracownik': pracownik,
+        'obiekty': obiekty,
+    }
+    return render(request, 'user_app/user_objects.html', context)
 
 
 def user_add_app(request):
-    return render(request, 'user_app/user_add_app.html')
+    pracownik_id = request.session['pracownik']
+    pracownik = Pracownik.objects.get(id=pracownik_id)
+    if request.method == 'POST':
+        form = AddApplicationForm(request.POST, initial={'pracownik': pracownik})
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Dodano wniosek')
+            return redirect('user_index')
+        else:
+            messages.warning(request, 'Popraw formularz')
+            return redirect('user_add_app')
+    else:
+        form = AddApplicationForm(initial={'pracownik': pracownik})
+    context = {
+        'pracownik': pracownik,
+        'form': form,
+    }
+    return render(request, 'user_app/user_add_app.html', context)
+
+
+def user_app_accepted(request):
+    pracownik_id = request.session['pracownik']
+    pracownik = Pracownik.objects.get(id=pracownik_id)
+    wnioski = Wniosek.objects.filter(pracownik=pracownik_id)
+    historia = Historia.objects.filter(wniosek__in=wnioski, status=1)
+    context = {
+        'pracownik': pracownik,
+        'historia': historia,
+    }
+    return render(request, 'user_app/user_app_accepted.html', context)
+
+
+def user_app_rejected(request):
+    pracownik_id = request.session['pracownik']
+    pracownik = Pracownik.objects.get(id=pracownik_id)
+    wnioski = Wniosek.objects.filter(pracownik=pracownik_id)
+    historia = Historia.objects.filter(wniosek__in=wnioski, status=2)
+    context = {
+        'pracownik': pracownik,
+        'historia': historia,
+    }
+    return render(request, 'user_app/user_app_rejected.html', context)
+
+
+def user_profile(request):
+    pracownik_id = request.session['pracownik']
+    pracownik = Pracownik.objects.get(id=pracownik_id)
+    context = {
+        'pracownik': pracownik,
+    }
+    return render(request, 'user_app/user_profile.html', context)
